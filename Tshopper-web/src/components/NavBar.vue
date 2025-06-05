d04edfbd4c2c75c5e10b0942e79159e40bcaa9d3
Michael Holley <michael.philipp.holley@gmail.com>
2025-06-05 18:02:56 +0200

    checked items indicator

--- Tshopper-web/src/components/NavBar.vue
+++ Tshopper-web/src/components/NavBar.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import { useShoppingListStore } from '@/stores/ShoppingListStore'
+import { computed } from 'vue'
+
+const store = useShoppingListStore()
+
+const checked = computed(() => store.items.filter((item) => item.checked).length)
+</script>
+
 <template>
   <div class="flex justify-between items-center gap-2 py-1">
     <div><h1 class="text-2xl font-bold text-primary-400">Tshopper</h1></div>
+    <div>
+      <span class="text-sm text-neutral-500">{{ checked }}/{{ store.items.length }}</span>
+    </div>
   </div>
 </template>
