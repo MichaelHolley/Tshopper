6b37a7596a900e811343b9aacfa9166202fc742c
Michael Holley <michael.philipp.holley@gmail.com>
2025-07-18 10:46:00 +0200

    fixing tab Navigation visibility if not logged in

--- Tshopper-web/src/App.vue
+++ Tshopper-web/src/App.vue
@@ -1,3 +1,12 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const isLogin = computed(() => route.path == '/login')
+</script>
+
 <template>
   <UApp>
     <div class="container mx-auto max-w-sm h-svh flex flex-col px-2">
@@ -5,7 +14,7 @@
       <div class="grow overflow-y-auto">
         <RouterView />
       </div>
-      <TabNavigation />
+      <TabNavigation v-if="!isLogin" />
     </div>
   </UApp>
 </template>
